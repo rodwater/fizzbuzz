263fce770fd189cc71bb60a746dfc71fa6c5eaf6
Rodwater <rodwater@gmail.com>
2016-05-10 15:43:28 -0700

    second commit

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
-for (i = 1, i <= 100, ++)
+
+
+for (var i = 1; i < 101; i++) {
+	if (i % 15 == 0)
+		document.write("FizzBuzz" + "<p>")
+	
+	else if (i % 3 == 0) 
+		document.write("fizz" + "<p>")
+	
+	else if (i % 5 == 0 )
+		document.write("buzz" + "<p>")
+	
+	else
+		document.write(i + "<p>")
+}
+
+
